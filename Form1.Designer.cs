c6eaa820cf7baa76af50241ba12a6585ae861154
revvilo <revvilo@protonmail.com>
2022-04-25 17:02:56 +1200

    Inital Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace Naughts_and_Crosses
+{
+    partial class NaughtsAndCrossesWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.boxTopRight = new System.Windows.Forms.PictureBox();
+            this.boxMidRight = new System.Windows.Forms.PictureBox();
+            this.boxMidMid = new System.Windows.Forms.PictureBox();
+            this.boxMidLeft = new System.Windows.Forms.PictureBox();
+            this.boxBotRight = new System.Windows.Forms.PictureBox();
+            this.boxBotMid = new System.Windows.Forms.PictureBox();
+            this.boxBotLeft = new System.Windows.Forms.PictureBox();
+            this.playerDisplay = new System.Windows.Forms.Label();
+            this.boxTopMid = new System.Windows.Forms.PictureBox();
+            this.boxTopLeft = new System.Windows.Forms.PictureBox();
+            this.restartButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.boxTopRight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boxMidRight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boxMidMid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boxMidLeft)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boxBotRight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boxBotMid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boxBotLeft)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boxTopMid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boxTopLeft)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // boxTopRight
+            // 
+            this.boxTopRight.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.boxTopRight.BackColor = System.Drawing.Color.LightGray;
+            this.boxTopRight.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.boxTopRight.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.boxTopRight.Location = new System.Drawing.Point(442, 88);
+            this.boxTopRight.Name = "boxTopRight";
+            this.boxTopRight.Size = new System.Drawing.Size(180, 180);
+            this.boxTopRight.TabIndex = 2;
+            this.boxTopRight.TabStop = false;
+            this.boxTopRight.Tag = "";
+            this.boxTopRight.Click += new System.EventHandler(this.boxTopRight_Click);
+            // 
+            // boxMidRight
+            // 
+            this.boxMidRight.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.boxMidRight.BackColor = System.Drawing.Color.LightGray;
+            this.boxMidRight.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.boxMidRight.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.boxMidRight.Location = new System.Drawing.Point(442, 290);
+            this.boxMidRight.Name = "boxMidRight";
+            this.boxMidRight.Size = new System.Drawing.Size(180, 180);
+            this.boxMidRight.TabIndex = 5;
+            this.boxMidRight.TabStop = false;
+            this.boxMidRight.Click += new System.EventHandler(this.boxMidRight_Click);
+            // 
+            // boxMidMid
+            // 
+            this.boxMidMid.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.boxMidMid.BackColor = System.Drawing.Color.LightGray;
+            this.boxMidMid.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.boxMidMid.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.boxMidMid.Location = new System.Drawing.Point(227, 290);
+            this.boxMidMid.Name = "boxMidMid";
+            this.boxMidMid.Size = new System.Drawing.Size(180, 180);
+            this.boxMidMid.TabIndex = 4;
+            this.boxMidMid.TabStop = false;
+            this.boxMidMid.Click += new System.EventHandler(this.boxMidMid_Click);
+            // 
+            // boxMidLeft
+            // 
+            this.boxMidLeft.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.boxMidLeft.BackColor = System.Drawing.Color.LightGray;
+            this.boxMidLeft.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.boxMidLeft.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.boxMidLeft.Location = new System.Drawing.Point(12, 290);
+            this.boxMidLeft.Name = "boxMidLeft";
+            this.boxMidLeft.Size = new System.Drawing.Size(180, 180);
+            this.boxMidLeft.TabIndex = 3;
+            this.boxMidLeft.TabStop = false;
+            this.boxMidLeft.Click += new System.EventHandler(this.boxMidLeft_Click);
+            // 
+            // boxBotRight
+            // 
+            this.boxBotRight.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.boxBotRight.BackColor = System.Drawing.Color.LightGray;
+            this.boxBotRight.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.boxBotRight.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.boxBotRight.Location = new System.Drawing.Point(442, 495);
+            this.boxBotRight.Name = "boxBotRight";
+            this.boxBotRight.Size = new System.Drawing.Size(180, 180);
+            this.boxBotRight.TabIndex = 8;
+            this.boxBotRight.TabStop = false;
+            this.boxBotRight.Click += new System.EventHandler(this.boxBotRight_Click);
+            // 
+            // boxBotMid
+            // 
+            this.boxBotMid.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.boxBotMid.BackColor = System.Drawing.Color.LightGray;
+            this.boxBotMid.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.boxBotMid.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.boxBotMid.Location = new System.Drawing.Point(227, 495);
+            this.boxBotMid.Name = "boxBotMid";
+            this.boxBotMid.Size = new System.Drawing.Size(180, 180);
+            this.boxBotMid.TabIndex = 7;
+            this.boxBotMid.TabStop = false;
+            this.boxBotMid.Click += new System.EventHandler(this.boxBotMid_Click);
+            // 
+            // boxBotLeft
+            // 
+            this.boxBotLeft.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.boxBotLeft.BackColor = System.Drawing.Color.LightGray;
+            this.boxBotLeft.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.boxBotLeft.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.boxBotLeft.Location = new System.Drawing.Point(12, 495);
+            this.boxBotLeft.Name = "boxBotLeft";
+            this.boxBotLeft.Size = new System.Drawing.Size(180, 180);
+            this.boxBotLeft.TabIndex = 6;
+            this.boxBotLeft.TabStop = false;
+            this.boxBotLeft.Click += new System.EventHandler(this.boxBotLeft_Click);
+            // 
+            // playerDisplay
+            // 
+            this.playerDisplay.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.playerDisplay.Font = new System.Drawing.Font("Poetsen One", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playerDisplay.Location = new System.Drawing.Point(12, 9);
+            this.playerDisplay.Name = "playerDisplay";
+            this.playerDisplay.Size = new System.Drawing.Size(610, 76);
+            this.playerDisplay.TabIndex = 9;
+            this.playerDisplay.Text = "Player\'s Turn";
+            this.playerDisplay.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // boxTopMid
+            // 
+            this.boxTopMid.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.boxTopMid.BackColor = System.Drawing.Color.LightGray;
+            this.boxTopMid.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.boxTopMid.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.boxTopMid.Location = new System.Drawing.Point(227, 88);
+            this.boxTopMid.Name = "boxTopMid";
+            this.boxTopMid.Size = new System.Drawing.Size(180, 180);
+            this.boxTopMid.TabIndex = 1;
+            this.boxTopMid.TabStop = false;
+            this.boxTopMid.Tag = "";
+            this.boxTopMid.Click += new System.EventHandler(this.boxTopMid_Click);
+            // 
+            // boxTopLeft
+            // 
+            this.boxTopLeft.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.boxTopLeft.BackColor = System.Drawing.Color.LightGray;
+            this.boxTopLeft.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.boxTopLeft.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.boxTopLeft.Location = new System.Drawing.Point(12, 88);
+            this.boxTopLeft.Name = "boxTopLeft";
+            this.boxTopLeft.Size = new System.Drawing.Size(180, 180);
+            this.boxTopLeft.TabIndex = 0;
+            this.boxTopLeft.TabStop = false;
+            this.boxTopLeft.Click += new System.EventHandler(this.boxTopLeft_Click);
+            // 
+            // restartButton
+            // 
+            this.restartButton.Font = new System.Drawing.Font("Poetsen One", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.restartButton.Location = new System.Drawing.Point(170, 324);
+            this.restartButton.Name = "restartButton";
+            this.restartButton.Size = new System.Drawing.Size(294, 106);
+            this.restartButton.TabIndex = 10;
+            this.restartButton.Text = "New Game";
+            this.restartButton.UseVisualStyleBackColor = false;
+            this.restartButton.Visible = false;
+            this.restartButton.Click += new System.EventHandler(this.restartButton_Click);
+            // 
+            // NaughtsAndCrossesWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 687);
+            this.Controls.Add(this.restartButton);
+            this.Controls.Add(this.playerDisplay);
+            this.Controls.Add(this.boxBotRight);
+            this.Controls.Add(this.boxBotMid);
+            this.Controls.Add(this.boxBotLeft);
+            this.Controls.Add(this.boxMidRight);
+            this.Controls.Add(this.boxMidMid);
+            this.Controls.Add(this.boxMidLeft);
+            this.Controls.Add(this.boxTopRight);
+            this.Controls.Add(this.boxTopMid);
+            this.Controls.Add(this.boxTopLeft);
+            this.Name = "NaughtsAndCrossesWindow";
+            this.Text = "WINDOWNAME";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.boxTopRight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boxMidRight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boxMidMid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boxMidLeft)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boxBotRight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boxBotMid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boxBotLeft)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boxTopMid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boxTopLeft)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox boxTopRight;
+        private System.Windows.Forms.PictureBox boxMidRight;
+        private System.Windows.Forms.PictureBox boxMidMid;
+        private System.Windows.Forms.PictureBox boxMidLeft;
+        private System.Windows.Forms.PictureBox boxBotRight;
+        private System.Windows.Forms.PictureBox boxBotMid;
+        private System.Windows.Forms.PictureBox boxBotLeft;
+        private System.Windows.Forms.Label playerDisplay;
+        private System.Windows.Forms.PictureBox boxTopMid;
+        private System.Windows.Forms.PictureBox boxTopLeft;
+        private System.Windows.Forms.Button restartButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,346 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Threading;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Naughts_and_Crosses
+{
+    public partial class NaughtsAndCrossesWindow : Form
+    {
+
+        public static string VERSION = "BETA v1.0";
+        public NaughtsAndCrossesWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        PictureBox[] tiles = new PictureBox[8];
+
+        // The Array for the possible wins
+        Control[][] winStates = new Control[8][];
+
+        //Creates the random number generator and the player variable (E Means error)
+        Random rand = new Random();
+        char player = 'E';
+        bool winState = false;
+        bool draw = false;
+
+
+        /// <summary>
+        /// Runs the load checks, randomises the player and displays it on startup
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Text = "Naughts and Crosses " + VERSION;
+            restartButton.Visible = false;
+
+            winStates[0] = new PictureBox[] { boxTopLeft, boxTopMid, boxTopRight };    //   _
+            winStates[1] = new PictureBox[] { boxMidLeft, boxMidMid, boxMidRight };    //   _
+            winStates[2] = new PictureBox[] { boxBotLeft, boxBotMid, boxBotRight };    //   _
+            winStates[3] = new PictureBox[] { boxTopLeft, boxMidLeft, boxBotLeft };    //   |..
+            winStates[4] = new PictureBox[] { boxTopMid, boxMidMid, boxBotMid };       //   .|.
+            winStates[5] = new PictureBox[] { boxTopRight, boxMidRight, boxBotRight }; //   ..|
+            winStates[6] = new PictureBox[] { boxTopLeft, boxMidMid, boxBotRight };    //   \
+            winStates[7] = new PictureBox[] { boxTopRight, boxMidMid, boxBotLeft };    //   /
+
+            int randomNum = rand.Next(2);
+
+            if (randomNum == 0)
+            {
+                player = 'O';
+            }
+            else if(randomNum == 1)
+            {
+                player = 'X';
+            }
+
+            playerDisplay.Text = "Player " + player + "'s turn";
+        }
+
+
+        /// <summary>
+        /// Flips to the other player
+        /// </summary>
+        /// <returns></returns>
+        public void flipPlayer()
+        {
+            if (player == 'X')
+            {
+                player = 'O';
+            }
+            else
+            {
+                player = 'X';
+            }
+        }
+
+
+        /// <summary>
+        /// Depending on the current player, place a Naught or Cross on the clicked picture box (Requires clicked picture box's code name)
+        /// </summary>
+        /// <param name="boxLoc"></param>
+        public void placeImage(PictureBox boxLoc)
+        {
+            if (player == 'X')
+            {
+                boxLoc.Image = Naughts_and_Crosses.Properties.Resources.Cross;
+                boxLoc.Tag = "Cross";
+
+            }
+            else if (player == 'O')
+            {
+                boxLoc.Image = Naughts_and_Crosses.Properties.Resources.Naught;
+                boxLoc.Tag = "Naught";
+
+            }
+            boxLoc.Enabled = false;
+
+            checkWin();
+
+            if(winState)
+            {
+                playerDisplay.Text = "Game Over - " + player + " WINS!";
+                disableTiles();
+                restartButton.Visible = true;
+                
+            }
+            else if (draw)
+            {
+                playerDisplay.Text = "Game Over - DRAW";
+                disableTiles();
+                restartButton.Visible = true;
+
+            }
+            else
+            {
+                flipPlayer();
+                playerDisplay.Text = "Player " + player + "'s turn";
+
+            }
+
+        }
+        
+
+
+        /// <summary>
+        /// Checks if the current arrangement of Naughts and Crosses equals a win or game over
+        /// </summary>
+        /// <returns></returns>
+        public bool checkWin()
+        {
+
+            // Runs through every possible arrangement of tiles to check for the win
+            foreach(Array state in winStates)
+            {
+
+                //=-=-=-=-= Cross checker
+
+                foreach(PictureBox box in state)
+                {
+                    if((string)box.Tag == "Cross")
+                    {
+                        winState = true;
+                    }
+                    else
+                    {
+                        winState = false;
+                        break;
+                    }
+                }
+
+                if (winState == true)
+                {
+                    return winState;
+                }
+
+
+                
+                //=-=-=-=-= Naught checker
+
+                foreach (PictureBox box in state)
+                {
+                    if((string)box.Tag == "Naught")
+                    {
+                        winState = true;
+                    }
+                    else
+                    {
+                        winState = false;
+                        break;
+                    }
+                }
+
+                if (winState == true)
+                {
+                    return winState;
+                }
+
+            } // End foreach possible win state
+
+
+
+            // And if no one has won, check if the board is full
+            draw = true;
+
+            foreach(Control c in Controls)
+            {
+                if(c is PictureBox)
+                {
+                    if (c.Enabled == true)
+                    {
+                        draw = false;
+                        break;
+                    }
+                }
+
+            } // End foreach control
+
+            return draw;
+        }
+
+        /// <summary>
+        /// Disables all the PictureBoxes
+        /// </summary>
+        public void disableTiles()
+        {
+            foreach(Control c in Controls)
+            {
+                if(c is PictureBox)
+                {
+                    c.Enabled = false;
+                }
+            }
+
+        }
+
+
+        /// <summary>
+        /// Enables all the PictureBoxes
+        /// </summary>
+        public void enableTiles()
+        {
+            foreach (Control c in Controls)
+            {
+                if (c is PictureBox)
+                {
+                    c.Enabled = true;
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Removes all Naughts and Crosses from the board
+        /// </summary>
+        public void clearTiles()
+        {
+            boxTopLeft.Image = null;
+            boxTopMid.Image = null;
+            boxTopRight.Image = null;
+            boxMidLeft.Image = null;
+            boxMidMid.Image = null;
+            boxMidRight.Image = null;
+            boxBotLeft.Image = null;
+            boxBotMid.Image = null;
+            boxBotRight.Image = null;
+
+            boxTopLeft.Tag = null;
+            boxTopMid.Tag = null;
+            boxTopRight.Tag = null;
+            boxMidLeft.Tag = null;
+            boxMidMid.Tag = null;
+            boxMidRight.Tag = null;
+            boxBotLeft.Tag = null;
+            boxBotMid.Tag = null;
+            boxBotRight.Tag = null;
+        }
+
+
+        /// <summary>
+        /// Restarts the game
+        /// </summary>
+        public void restart()
+        {
+            restartButton.Visible = false;
+            clearTiles();
+            enableTiles();
+            Form1_Load(null, null);
+
+        }
+
+
+        //All the events that get triggered by clicking the picture boxes
+        #region clickEvents
+
+        private void boxTopLeft_Click(object sender, EventArgs e)
+        {
+            placeImage(boxTopLeft);
+
+        }
+
+        private void boxTopMid_Click(object sender, EventArgs e)
+        {
+            placeImage(boxTopMid);
+
+        }
+
+        private void boxTopRight_Click(object sender, EventArgs e)
+        {
+            placeImage(boxTopRight);
+
+
+        }
+
+        private void boxMidLeft_Click(object sender, EventArgs e)
+        {
+            placeImage(boxMidLeft);
+
+        }
+
+        private void boxMidMid_Click(object sender, EventArgs e)
+        {
+            placeImage(boxMidMid);
+
+        }
+
+        private void boxMidRight_Click(object sender, EventArgs e)
+        {
+            placeImage(boxMidRight);
+
+        }
+
+        private void boxBotLeft_Click(object sender, EventArgs e)
+        {
+            placeImage(boxBotLeft);
+
+        }
+
+        private void boxBotMid_Click(object sender, EventArgs e)
+        {
+            placeImage(boxBotMid);
+
+        }
+
+        private void boxBotRight_Click(object sender, EventArgs e)
+        {
+            placeImage(boxBotRight);
+
+        }
+
+        #endregion
+
+        private void restartButton_Click(object sender, EventArgs e)
+        {
+            restart();
+        }
+    }
+}
